2e15ac3f0c47941a51b4eff39445f0f19a4c5494
Mego <mega.gamer05@gmail.com>
2017-11-05 14:37:55 -0600

    Basic post template rendering

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
+import mysql.connector
 from flask import Flask, render_template
 
-app = Flask("PPCG v2")
+import config
 
+app = Flask("PPCG v2")
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 
+@app.route("/post/<int:post_id>/<post_title>")
+def get_post(post_id, post_title):
+    cursor = db_conn.cursor()
+    post_data = next(cursor.execute("SELECT * FROM posts WHERE id = %s", (post_id,)))
+    cursor.close()
+    return render_template('post.html', post_data=post_data)
+
+
 if __name__ == '__main__':
-    app.run()
+    db_conn = None
+    try:
+        db_conn = mysql.connector.connect(**config.db_config)
+        app.run()
+    finally:
+        if db_conn is not None:
+            db_conn.close()
