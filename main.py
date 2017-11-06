c53aa150a8aa58c0cf2c559a3b3f702db9a6796b
Mego <mega.gamer05@gmail.com>
2017-11-05 22:23:30 -0600

    Setup posts GET endpoint, add requirements.txt

--- main.py
+++ main.py
@@ -6,25 +6,32 @@ from flask import Flask, render_template
 import config
 
 app = Flask("PPCG v2")
+db_conn = None
+
+
+def next_row_to_dict(cursor):
+    return dict(zip(cursor.column_names, cursor.fetchone()))
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 
+@app.route("/post/<int:post_id>")
 @app.route("/post/<int:post_id>/<post_title>")
-def get_post(post_id, post_title):
+def get_post(post_id, post_title=None):
     cursor = db_conn.cursor()
-    post_data = next(cursor.execute("SELECT * FROM posts WHERE id = %s", (post_id,)))
+    cursor.execute("SELECT * FROM posts WHERE id = %s", (post_id,))
+    post_data = next_row_to_dict(cursor)
     cursor.close()
     return render_template('post.html', post_data=post_data)
 
 
 if __name__ == '__main__':
-    db_conn = None
     try:
         db_conn = mysql.connector.connect(**config.db_config)
-        app.run()
+        app.run(host='127.0.0.1', port=5000)
     finally:
         if db_conn is not None:
             db_conn.close()
