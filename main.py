0b1bcfb730c4e150c5c842836385550b2ed21ed4
Pavel Braginskiy <pavelyay@gmail.com>
2017-11-06 20:24:27 -0800

    Add 404 page, attempting to access a nonexistant post displays it.

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def get_post(post_id, post_title=None):
     cursor.execute("SELECT * FROM posts WHERE id = %s", (post_id,))
     post_data = cursor.fetchone()
     cursor.close()
+    if post_data is None:
+        return render_template('notfound.html'), 404
     return render_template('post.html', post_data=post_data)
 
 
