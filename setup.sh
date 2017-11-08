b703695dc25b745a74c5b3d92f046b6a5abc71d6
Vihan B <contact@vihan.org>
2017-11-07 22:16:44 -0800

    Minor tweaks
    Signed-off-by: Vihan B <contact@vihan.org>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-read -p "Install dependencies (yn): " should_deps;
-if [[ $should_deps = "y" ]]; then
+read -p "Install gems (yn): " should_gems
+if [[ $should_gems = "y" ]]; then
     gem install sass
-    npm i -g autoprefixer postcss-cli clean-css@3.4.24
-fi;
+fi
 
-read -p "Setup MySQL (yn): " should_mysql;
+read -p "Install npm packages (yn): " should_npm
+if [[ $should_npm = "y" ]]; then
+    npm i -g autoprefixer postcss-cli clean-css@3.4.24
+fi
+
+read -p "Install python packages (yn): " should_pip
+if [[ $should_pip = "y" ]]; then
+    pip install -r requirements.txt
+fi
+
+read -p "Setup MySQL (yn): " should_mysql
 if [[ $should_mysql == "y" ]]; then
     read -p "MySQL username: " sql_username
     read -s -p "MySQL password: " sql_username
