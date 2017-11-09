c13e3bb430a92159bef174d1ad6f024e63d7f6a7
pavelbraginskiy <pavelbraginskiy@hotmail.com>
2017-11-09 10:21:06 -0800

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,5 @@
-import os
-
-__all__ = []
-
-
-# This seems like a hack but it doesn't work with the default __all__ and this way this file doesn't
-# Need to be updated for each new route module.
-for i in os.listdir('./app/routes'):
-    if i != '__init__.py' and i.endswith('.py'):
-        __all__.append(i[0:-3])
+__all__ = [
+    'post',
+    'answer',
+    'static'
+]
