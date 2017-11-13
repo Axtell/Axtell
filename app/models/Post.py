43c73e35951091bbaa27ae23e2ed381c9fea34d0
Vihan B <contact@vihan.org>
2017-11-12 22:15:56 -0800

    Add reaaallly basic front-end for posts
    Signed-off-by: Vihan B <contact@vihan.org>

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,6 +1,7 @@
 from app.instances.db import db
 from app.models.User import User
 from sqlalchemy.dialects.mysql import LONGTEXT
+import datetime
 
 class Post(db.Model):
     """
@@ -10,6 +11,9 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(LONGTEXT, nullable=False)
+    
+    
+    date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     
     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     user = db.relationship(User, backref=db.backref('posts'))
