fd177d62c742edb6a6f43592390acc664cb570f3
Mego <mega.gamer05@gmail.com>
2017-11-16 10:17:28 -0600

    Make Celery do Markdown rendering, cleanup

--- app/helpers/tasks.py
+++ app/helpers/tasks.py
@@ -4,6 +4,9 @@ from celery import Celery
 from app.instances import db
 from app.models import User
 from config import redis_config
+from os import path
+from subprocess import Popen, PIPE
+from struct import unpack
 
 redis_url = f"redis://:{redis_config['password']}@{redis_config['host']}:{redis_config['port']}/{redis_config['db']}"
 
@@ -13,12 +16,15 @@ celery_app = Celery(
     backend=redis_url
 )
 db_session = None
+render_proc = None
 
 
 @celery_app.task
 def init():
-    global db_session
+    global db_session, render_proc
     db_session = db.db.session()
+    exec_path = path.join(path.dirname(path.realpath(__file__)), 'markdown', 'markdown.js')
+    render_proc = Popen(['node', exec_path], stdout=PIPE, stdin=PIPE)
 
 
 @celery_app.task
@@ -28,3 +34,11 @@ def create_user(name, email):
     db_session.add(new_user)
     db_session.commit()
     return new_user.to_json()
+
+
+@celery_app.task
+def render_markdown(string):
+    render_proc.stdin.write(string.encode('utf-8'))
+    render_proc.stdin.flush()
+    read_len, = unpack('<i', render_proc.stdout.read(4))
+    return render_proc.stdout.read(read_len).decode('utf-8')
