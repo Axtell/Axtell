fd15d44a4f1d3d5350d3822fa644fcf9aa99f60c
Ryan R <mega.gamer05@gmail.com>
2017-11-18 14:14:06 -0600

    more celery fixes

--- app/helpers/tasks.py
+++ app/helpers/tasks.py
@@ -4,7 +4,6 @@ from celery import Celery
 from config import redis_config
 from os import path
 from subprocess import Popen, PIPE, run
-from struct import unpack
 
 redis_url = f"redis://:{redis_config['password']}@{redis_config['host']}:{redis_config['port']}/{redis_config['db']}"
 
@@ -16,19 +15,12 @@ celery_app = Celery(
 db_session = None
 render_proc = None
 
-
-@celery_app.task
-def init():
-    global db_session, render_proc
-    import app.instances.db
-    db_session = app.instances.db.db.session()
-    exec_path = path.join(path.dirname(path.realpath(__file__)), 'markdown', 'markdown.js')
-    render_proc = Popen(['node', exec_path], stdout=PIPE, stdin=PIPE)
-
-
 @celery_app.task
 def create_user(name, email):
     global db_session
+    if db_session is None:
+        import app.instances.db
+        db_session = app.instances.db.db.session()
     import app.models.User
     new_user = app.models.User.User(name=name, email=email)
     db_session.add(new_user)
@@ -38,9 +30,13 @@ def create_user(name, email):
 
 @celery_app.task
 def render_markdown(string):
+    global render_proc
+    if render_proc is None:
+        exec_path = path.join(path.dirname(path.realpath(__file__)), 'markdown', 'markdown.js')
+        render_proc = Popen(['node', exec_path], stdout=PIPE, stdin=PIPE, stderr=None)
     render_proc.stdin.write(string.encode('utf-8'))
     render_proc.stdin.flush()
-    read_len, = unpack('<i', render_proc.stdout.read(4))
+    read_len = int.from_bytes(render_proc.stdout.read(4), byteorder='little')
     return render_proc.stdout.read(read_len).decode('utf-8')
 
 
