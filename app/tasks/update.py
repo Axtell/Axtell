f2c60ef7aafaf99b955d0ed8d693e63b63a0286b
Mego <mega.gamer05@gmail.com>
2017-11-19 07:20:30 -0600

    Render Markdown within Python to avoid Celery pipe issues hopefully

--- app/tasks/update.py
+++ app/tasks/update.py
@@ -1,6 +1,7 @@
 from app.instances.celery import celery_app
 from subprocess import Popen, PIPE, run
 
+
 @celery_app.task
 def update(commit):
     return run(["git", "checkout", commit]).returncode == 0
