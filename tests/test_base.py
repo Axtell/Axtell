445b54ce1952f72433c8536c0a2e01c5da73ab10
Mego <mega.gamer05@gmail.com>
2017-11-20 09:15:43 -0600

    Fix imports in tests - what did I do to deserve this hell

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,17 +1,19 @@
 import unittest
-from app.start import server, db
+import app.start
+import app.instances.db
+
 
 class TestBase:
     class TestFlask(unittest.TestCase):
         def setUp(self):
             super().setUp()
-            self.app = server.test_client()
+            self.app = app.start.server.test_client()
             self.app.testing = True
 
     class TestDB(unittest.TestCase):
         def setUp(self):
             super().setUp()
-            self.session = db.session
+            self.session = app.instances.db.db.session
             self.session.begin_nested()
             
         def tearDown(self):
