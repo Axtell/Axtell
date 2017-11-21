909787fcd78f4a568e26195fb4bd8b7f457045dd
Mego <mega.gamer05@gmail.com>
2017-11-20 18:32:09 -0600

    Fix import again

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,14 +1,14 @@
 from flask import g
 
 from app.helpers.render import render_json, render_error
-from app.models.User import User
+import app.models.User
 
 
 def get_my_profile():
     """
     Returns the logged in user's profile or a JSON with `unauthorized: true`
     """
-    if isinstance(g.user, User):
+    if isinstance(g.user, app.models.User.User):
         return render_json(g.user.to_json())
     else:
         return render_json({ 'unauthorized': True })
@@ -18,7 +18,7 @@ def get_profile(user_id):
     """
     Returns a user's user_id
     """
-    user = User.query.filter_by(id=user_id).first()
+    user = app.models.User.User.query.filter_by(id=user_id).first()
     
     if user is None:
         return render_error('user not found'), 400
