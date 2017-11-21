ce23c05e5da453abf7e215c67015cbb9b823c28f
Mego <mega.gamer05@gmail.com>
2017-11-20 21:29:43 -0600

    Merge markdown changes from master

--- app/tasks/markdown.py
+++ app/tasks/markdown.py
@@ -1,24 +1,7 @@
-from subprocess import Popen, PIPE
-from threading import local
-
 from app.instances.celery import celery_app
-from misc import md_exe
+import markdown
 
-markdown_local = local()
-
-def fork_helper():
-    if not (hasattr(markdown_local, 'render_proc') and markdown_local.render_proc is not None):
-        markdown_local.render_proc = Popen(['node', md_exe], stdout=PIPE, stdin=PIPE)
-    
-    return markdown_local.render_proc
 
 @celery_app.task
 def render_markdown(string):
-    helper = fork_helper()
-    
-    helper.stdin.write(string.encode('utf-8'))
-    helper.stdin.flush()
-    read_len = int.from_bytes(helper.stdout.read(4), byteorder='little')
-    res = helper.stdout.read(read_len).decode('utf-8')
-    
-    return res
+    return markdown.markdown(string)
