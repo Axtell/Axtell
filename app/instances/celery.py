4a13a79946d265de9789b55df0214f6b545a7ddc
Mego <mega.gamer05@gmail.com>
2017-11-23 17:17:47 -0600

    Use memcached for results backend to cache results

--- app/instances/celery.py
+++ app/instances/celery.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from celery import Celery
-from config import redis_config
+from config import redis_config, memcached_config
 
 redis_url = f"redis://:{redis_config['password']}@{redis_config['host']}:{redis_config['port']}/{redis_config['db']}"
+memcached_url = f"cache+memcached://{memcached_config['host']}:{memcached_config['port']}/"
 
 celery_app = Celery(
     "celery_server",
     broker=redis_url,
-    backend=redis_url
+    backend=memcached_url
 )
