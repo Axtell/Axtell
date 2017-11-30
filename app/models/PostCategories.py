008d89d057e7a1a9cbbeb9412c9fc14e483af38e
Mego <mega.gamer05@gmail.com>
2017-11-29 19:59:48 -0600

    Make post_categories a function to delay loading until after posts and categories are loaded

--- app/models/PostCategories.py
+++ app/models/PostCategories.py
@@ -1,8 +1,10 @@
 from app.instances.db import db, DBBase
 
-categories = db.Table(
-    'post_categories',
-    DBBase.metadata,
-    db.Column('category_name', db.Integer, db.ForeignKey('categories.name'), primary_key=True),
-    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
-)
+
+def categories():
+    return db.Table(
+        'post_categories',
+        DBBase.metadata,
+        db.Column('category_name', db.Integer, db.ForeignKey('categories.name'), primary_key=True),
+        db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    )
