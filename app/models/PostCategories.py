d3767be6ee4a91b1ba967868c7e0c616c327d80b
Mego <mega.gamer05@gmail.com>
2017-11-29 18:46:22 -0600

    Fix DB names

--- app/models/PostCategories.py
+++ app/models/PostCategories.py
@@ -1,6 +1,6 @@
 from app.instances.db import db
 
 categories = db.Table(
-    db.Column('category_name', db.Integer, db.ForeignKey('category.name'), primary_key=True),
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    db.Column('category_name', db.Integer, db.ForeignKey('categories.name'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
 )
