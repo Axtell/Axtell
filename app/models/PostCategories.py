d39fcad397094d86d6eac563560984d0db45f3d7
Mego <mega.gamer05@gmail.com>
2017-11-29 19:14:44 -0600

    Correctly implement relationship table

--- app/models/PostCategories.py
+++ app/models/PostCategories.py
@@ -1,7 +1,9 @@
 from app.instances.db import DBBase
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
-categories = DBBase.Table(
+categories = Table(
+    'post_categories',
+    DBBase.metadata,
     Column('category_name', Integer, ForeignKey('categories.name'), primary_key=True),
     Column('post_id', Integer, ForeignKey('posts.id'), primary_key=True)
 )
