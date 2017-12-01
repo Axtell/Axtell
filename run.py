50c9cfbe24bc99e0a3ae1ce7ec842749712cf466
Ryan R <mega.gamer05@gmail.com>
2017-11-30 23:11:51 -0600

    modify run.py to work with nginx and uwgsi

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import config
-from app.start import run
+from app.start import run, server
+
+def run_server(host=config.app['host'], port=config.app['port']):
+    run(host, port)
 
 if __name__ == '__main__':
-    run(host=config.app['host'], port=config.app['port'])
+    run_server()
