6d823fda3da2220a1a02293fa19665d70bab89c7
Mego <mega.gamer05@gmail.com>
2017-12-07 03:44:53 -0600

    Primary keys are important

--- app/models/AnswerVote.py
+++ app/models/AnswerVote.py
@@ -6,8 +6,8 @@ class AnswerVote(db.Model):
     __tablename__ = 'answer_votes'
     __table_args__ = {'extend_existing': True}
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'), primary_key=True)
     vote = db.Column(TINYINT, default=0)
 
     user = db.relationship('User', backref='votes')
