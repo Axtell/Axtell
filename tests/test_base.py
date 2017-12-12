6d3e171dc2e96e6843f3f2be568665fed34de36f
Mego <mega.gamer05@gmail.com>
2017-12-12 12:13:36 -0600

    Use existing db object

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,27 +1,22 @@
 import unittest
 
 from app.start import server
+from app.instances.db import db
 from flask_testing import TestCase
 from flask_sqlalchemy import SQLAlchemy
 from config import db_config
 
 
 class TestFlask(TestCase):
-    SQLALCHEMY_DATABASE_URI = \
-        f"mysql+mysqlconnector://" \
-        f"{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/" \
-        f"{db_config['database']}_test?charset=utf8mb4"
 
     def create_app(self):
         app = server
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = self.SQLALCHEMY_DATABASE_URI
         return app
 
     def setUp(self):
         super().setUp()
-        self.db = SQLAlchemy(self.app)
-        self.db.init_app(self.app)
+        self.db = db
         self.db.create_all()
         self.session = self.db.session()
         self.session.begin_nested()
