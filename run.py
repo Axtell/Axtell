e83135868a19854b78ed605c66e874da319cf39b
Ryan R <Mego@users.noreply.github.com>
2017-12-27 11:11:53 -0600

    Do logging

--- run.py
+++ run.py
@@ -3,6 +3,13 @@ import config
 from app.start import run, server
 from app.routes import *
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+handler = RotatingFileHandler('ppcg-v2.log', maxBytes=10000, backupCount=3)
+handler.setLevel(logging.INFO)
+server.logger.addHandler(handler)
+
 def run_server(host=config.app['host'], port=config.app['port']):
     run(host, port)
 
