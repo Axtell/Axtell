25f845442e7b4dd9465f60fc6fb840051f82294f
Ryan R <Mego@users.noreply.github.com>
2017-12-27 11:15:17 -0600

    Fix import?

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import config
-from app.start import run, server
+import app.start
 from app.routes import *
+
+server = app.start.server
 
 import logging
 from logging.handlers import RotatingFileHandler
@@ -11,7 +13,7 @@ handler.setLevel(logging.INFO)
 server.logger.addHandler(handler)
 
 def run_server(host=config.app['host'], port=config.app['port']):
-    run(host, port)
+    app.start.run(host, port)
 
 if __name__ == '__main__':
     run_server()
