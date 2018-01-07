62d436aa38335b2b9db505fc7fcfc8a31eb142b1
Vihan <contact@vihan.org>
2018-01-07 11:55:03 -0800

    Add namespaced SVG
    Signed-off-by: Vihan <contact@vihan.org>

--- static/js/controllers/VoteViewController.js
+++ static/js/controllers/VoteViewController.js
@@ -6,6 +6,15 @@ import ErrorManager from '~/helper/ErrorManager';
 export const VOTE_ACTIVE_CLASS = 'selected';
 export const VoteFailed = Symbol('AnswerVote.Error.RequestFailed');
 
+const LoadingIcon = (
+    <svg namespace="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 42 42">
+        <path namespace="http://www.w3.org/2000/svg" opacity=".2" d="M20.201 5.169c-8.254 0-14.946 6.692-14.946 14.946 0 8.255 6.692 14.946 14.946 14.946s14.946-6.691 14.946-14.946c-.001-8.254-6.692-14.946-14.946-14.946zm0 26.58c-6.425 0-11.634-5.208-11.634-11.634 0-6.425 5.209-11.634 11.634-11.634 6.425 0 11.633 5.209 11.633 11.634 0 6.426-5.208 11.634-11.633 11.634z"/>
+        <path namespace="http://www.w3.org/2000/svg" d="M26.013 10.047l1.654-2.866a14.855 14.855 0 0 0-7.466-2.012v3.312c2.119 0 4.1.576 5.812 1.566z">
+            <animateTransform namespace="http://www.w3.org/2000/svg" attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/>
+        </path>
+    </svg>
+);
+
 /**
  * Controls voting
  */
@@ -27,7 +36,7 @@ export default class VoteViewController extends ViewController {
         this._voteTotal = voteButton.getElementsByClassName('vote-count')[0];
 
         this._voteIcon = voteButton.getElementsByTagName('svg')[0];
-        this._loadingIcon = <img src={Theme.current.imageForTheme('loading')} />;
+        this._loadingIcon = LoadingIcon.cloneNode(true);
         this._iconParent = this._voteIcon.parentNode;
 
         this._isLoading = false;
@@ -106,6 +115,9 @@ export default class VoteViewController extends ViewController {
      * Toggles vote controller value.
      */
     toggleState() {
+        // Don't toggle if loading
+        if (this._isLoading) return;
+
         this.setVote(!this._isActive);
     }
 }
