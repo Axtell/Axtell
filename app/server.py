7e1de1175501eaa9ba4d9f3a90cd68d15eb7c346
Vihan <contact@vihan.org>
2018-01-06 20:04:03 -0800

    Better compression
    Signed-off-by: Vihan <contact@vihan.org>

--- app/server.py
+++ app/server.py
@@ -44,7 +44,7 @@ css_bundle_style('dark')
 # JS
 js = Bundle('js/main.js', filters=('browserify', 'uglifyjs'), output='lib/main.js')
 
-uglify_args = []
+uglify_args = ['-m', '--mange-props', 'regex=/^_.+$/', '-c']
 
 if server.debug:
     js.config['BROWSERIFY_EXTRA_ARGS'] = ['--debug']
