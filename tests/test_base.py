2d7b7a814ff0b759df8cc1d1bffd7da99ba5e7c3
Ryan R <Mego@users.noreply.github.com>
2018-01-16 05:43:52 -0600

    Add light and dark themes in test setup for travis

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,6 @@
 import app.start
 import app.instances.db
+from app.models.Theme import Theme
 from flask_testing import TestCase
 
 
@@ -19,6 +20,19 @@ class TestFlask(TestCase):
         self.db = app.instances.db.db
         self.session = self.db.session
         self.session.begin_nested()
+        
+        self.session.begin_nested()
+        light_theme = Theme(name='light')
+        try:
+            self.session.add(light_theme)
+        except:
+            pass
+        dark_theme = Theme(name='dark')
+        try:
+            self.session.add(dark_theme)
+        except:
+            pass
+        self.session.commit()
 
     def tearDown(self):
         super().tearDown()
