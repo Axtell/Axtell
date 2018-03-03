1518ceb93e111671e2f35317760dc08c95a02285
Mego <mega.gamer05@gmail.com>
2018-03-03 15:34:18 -0600

    Email is optional for gravatar, oauth returns None for email

--- app/helpers/macros/gravatar.py
+++ app/helpers/macros/gravatar.py
@@ -1,8 +1,13 @@
 from hashlib import md5
+import random
 
 
 def gravatar(email, size=128, rating='g', default='identicon', force_default=False):
-    hashemail = md5(email.encode('utf-8')).hexdigest()
+    if email is None:
+        email_bytes = bytes([random.randint(0, 255) for _ in range(256)])
+    else:
+        email_bytes = email.encode('utf-8')
+    hashemail = md5(email_bytes).hexdigest()
 
     link = "https://secure.gravatar.com/avatar/{hashemail}?s={size}&d={default}&r={rating}".format(
         hashemail=hashemail, size=size,
--- app/helpers/oauth.py
+++ app/helpers/oauth.py
@@ -1,15 +1,13 @@
-from app.models.User import UserOAuthToken
 import requests
-from json import loads as json_parse
 from config import oauth
 
-# ?order=desc&sort=reputation&site=codegolf&filter=!T6o*9Tv*x)DWaQr(r5
+
 def stackexchange(auth_token):
     user = requests.get(
         'https://api.stackexchange.com/2.2/me',
         params={
             'site': 'codegolf',
-            'filter': '!)RwcIFN1JaCrhVpgyYeR_oO*', # Constant obtained from SE API explorer
+            'filter': '!)RwcIFN1JaCrhVpgyYeR_oO*',  # Constant obtained from SE API explorer
             'access_token': auth_token,
             'key': oauth.get('se').get('key')
         }
@@ -17,5 +15,5 @@ def stackexchange(auth_token):
     return user.get('user_id'), {
         'name': user.get('display_name'),
         'avatar': user.get('profile_image'),
-        'email':'NULL@example.com'
+        'email': None
     }
