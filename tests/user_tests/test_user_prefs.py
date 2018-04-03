273a0ee205762029a4a5568df7c54b412587dc9d
Ryan R <Mego@users.noreply.github.com>
2018-04-02 23:24:51 -0500

    Update test_user_prefs.py

--- tests/user_tests/test_user_prefs.py
+++ tests/user_tests/test_user_prefs.py
@@ -56,7 +56,7 @@ class TestUserPrefs(TestFlask):
         dark_theme = Theme.query.filter_by(id=self.user.theme).first()
         self.assertEqual(dark_theme.name, "dark")
 
-        self.client.get("/index.html")  # make sure the dark theme files get generated
+        self.client.get("/")  # make sure the dark theme files get generated
 
         response2 = self.client.post("/theme/light", data={})
         self.assertEqual(response2.status_code, 204)
