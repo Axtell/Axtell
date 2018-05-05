cdb8c703c1633281f40f6ae4b8ecd23398b9f6a3
Vihan <contact@vihan.org>
2018-05-05 13:18:29 -0700

    Remove old WebAssets code
    Signed-off-by: Vihan <contact@vihan.org>

--- app/server.py
+++ app/server.py
@@ -27,23 +27,3 @@ update.jwt_update.delay().wait()
 @server.before_request
 def before_request():
     g.request_start_time = time()
-
-
-# CSS
-
-# bundle.config['AUTOPREFIXER_BROWSERS'] = ['Safari >= 8', '> 1%']
-# assets.register(f'css_{type}_all', bundle)
-
-# js_envs = {
-#     'GAPI_KEY': config.auth['google']['client-id'],
-#     'IMGUR_CLIENT_ID': config.auth['imgur']['client-id'],
-#     'POST_TITLE_MIN': str(config.posts['min_title']),
-#     'POST_TITLE_MAX': str(config.posts['max_title']),
-#     'POST_BODY_MIN': str(config.posts['min_len']),
-#     'POST_BODY_MAX': str(config.posts['max_len']),
-#     'MIN_USERNAME_LENGTH': str(config.users['min_name_len']),
-#     'MAX_USERNAME_LENGTH': str(config.users['max_name_len']),
-#     'MIN_COMMENT_LENGTH': str(config.comments['min_len']),
-#     'MAX_COMMENT_LENGTH': str(config.comments['max_len']),
-#     'IS_DEBUG': 'true' if server.debug else ''
-# }
