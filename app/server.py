fadd4a548b9a38c2fcb0c6462a6205756dc6ddb0
Mego <mega.gamer05@gmail.com>
2018-05-28 14:31:27 -0500

    fix encoding stuff

--- app/server.py
+++ app/server.py
@@ -7,8 +7,11 @@ import app.tasks.update as update
 from shutil import which
 import bugsnag
 from bugsnag.flask import handle_exceptions
-
+import golflang_encodings
 import config
+
+
+golflang_encodings.add_encodings.register_codecs()
 
 
 class AxtellFlask(Flask):
