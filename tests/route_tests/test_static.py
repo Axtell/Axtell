ddcd7d1856a6a25fc5b85e5af2544598fd83d541
Mego <mega.gamer05@gmail.com>
2018-07-12 09:10:56 -0500

    Handle native encodings

--- tests/route_tests/test_static.py
+++ tests/route_tests/test_static.py
@@ -3,6 +3,8 @@ from tests.test_base import TestFlask
 # noinspection PyUnresolvedReferences
 import app.routes.static
 
+import golflang_encodings
+
 
 class TestStatic(TestFlask):
     def test_home_loading(self):
@@ -14,3 +16,8 @@ class TestStatic(TestFlask):
         result = self.client.get('/404')
         self.assertEqual(result.status_code, 404)
         self.assertTrue(result.content_type.startswith('text/html'))
+
+    def test_codepage(self):
+        result = self.client.get('/static/encodings/jelly')
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.json['jelly'], golflang_encodings.add_encodings.codepages.get('Jelly'))
