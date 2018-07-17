1e5ac66b4618c226e1be039e0f70094bf7ffb6a9
Vihan <contact@vihan.org>
2018-07-16 20:24:40 -0700

    Move to modern views and fix bug
    Signed-off-by: Vihan <contact@vihan.org>

--- js/ui/login.js
+++ js/ui/login.js
@@ -1,8 +1,9 @@
 import AuthModalTemplate from '~/template/login/AuthModalTemplate';
-import ModalController from '~/controllers/ModalController';
+import ModalViewController from '~/controllers/ModalViewController';
 import Analytics, { EventType } from '~/models/Analytics';
 
+const authModal = new AuthModalTemplate();
 document.getElementById("am-login")?.addEventListener("click", (event) => {
     Analytics.shared?.report(EventType.loginOpen);
-    ModalController.shared.present(AuthModalTemplate.shared);
+    ModalViewController.shared.present(authModal);
 }, false);
