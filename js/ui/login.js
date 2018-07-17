c91b32731a32e481f8cb309ffdd1ec424586b33a
Vihan <contact@vihan.org>
2018-07-16 20:51:15 -0700

    Color modifiation
    Signed-off-by: Vihan <contact@vihan.org>

--- js/ui/login.js
+++ js/ui/login.js
@@ -2,8 +2,11 @@ import AuthModalTemplate from '~/template/login/AuthModalTemplate';
 import ModalViewController from '~/controllers/ModalViewController';
 import Analytics, { EventType } from '~/models/Analytics';
 
-const authModal = new AuthModalTemplate();
-document.getElementById("am-login")?.addEventListener("click", (event) => {
-    Analytics.shared?.report(EventType.loginOpen);
-    ModalViewController.shared.present(authModal);
-}, false);
+const authButton = document.getElementById("am-login");
+if (authButton) {
+    const authModal = new AuthModalTemplate();
+    authButton.addEventListener("click", (event) => {
+        Analytics.shared?.report(EventType.loginOpen);
+        ModalViewController.shared.present(authModal);
+    }, false);
+}
