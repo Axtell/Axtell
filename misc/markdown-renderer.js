c56116670ba4f3d42631a490ab9a9703c28b2374
Vihan <vihanb@users.noreply.github.com>
2018-07-23 18:10:01 -0700

    Add figures (#203)
    * Block images are now <figure>s
    
    Signed-off-by: Vihan <contact@vihan.org>
    
    * Fix bug
    
    Signed-off-by: Vihan <contact@vihan.org>

--- misc/markdown-renderer.js
+++ misc/markdown-renderer.js
@@ -3,12 +3,17 @@ var MarkdownIt = require('markdown-it'),
     hljs = require('highlight.js'),
     footnote = require('markdown-it-footnote'),
     highlight = require('./hljs-renderer'),
-    mila = require('markdown-it-link-attributes');
+    mila = require('markdown-it-link-attributes'),
+    implicitFigures = require('markdown-it-implicit-figures');
 
 var md = new MarkdownIt({
     html: false, // No html = no xss
     linkify: true,
     highlight: highlight
+}).use(implicitFigures, {
+    dataType: true,
+    figcaption: true,
+    link: true
 }).use(mk, {
     throwOnError: false,
     errorColor: "#F00"
