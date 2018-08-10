120502c1b19c7650635b4edbd69cfaf31720acfd
Vihan <contact@vihan.org>
2018-08-09 17:11:27 -0700

    Throw in some MathQuill changes
    Signed-off-by: Vihan <contact@vihan.org>

--- js/template/MathEditorModalTemplate.js
+++ js/template/MathEditorModalTemplate.js
@@ -6,6 +6,9 @@ import { MathQuill } from '~/helpers/LazyLoad';
 import LoadingIcon from '~/svg/LoadingIcon';
 import Theme from '~/models/Theme';
 
+// All greek letters that are greek-specific
+export const GREEK_LETTERS = "alpha beta Gamma gamma Lambda lambda Delta delta epsilon zeta eta Theta theta iota kappa mu nu Xi xi Pi pi Psi psi rho Sigma sigma tau Upsilon upsilon Phi phi chi Omega omega";
+
 /**
  * Math Editor Modal dialog.
  * @extends {ModalTemplate}
@@ -42,7 +45,9 @@ export default class MathEditorModalTemplate extends ModalTemplate {
     async willLoad() {
         const MQ = await MathQuill();
 
-        const editor = MQ.MathField(<div class="text-base text-input text-input--pad-all"></div>);
+        const editor = MQ.MathField(<div class="text-base text-input text-input--pad-all"></div>, {
+            autoCommands: `sum sqrt prod mid forall exists implies iff ldots int ${GREEK_LETTERS} pm gt lt ge le times div cdot langle rangle partial equiv`
+        });
         this._loadingSign.parentNode.replaceChild(editor.el(), this._loadingSign);
 
         this._addButton.setLoadingState(false);
