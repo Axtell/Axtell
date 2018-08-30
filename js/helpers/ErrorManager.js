7982f7e2d5472293982e7c9c37398bcd081ea570
Vihan <contact@vihan.org>
2018-08-29 22:17:07 -0700

    hotfix: (more) gracefully handle bugsnag block
    Signed-off-by: Vihan <contact@vihan.org>

--- js/helpers/ErrorManager.js
+++ js/helpers/ErrorManager.js
@@ -10,17 +10,21 @@ export let Bugsnag = null;
 
 const BugsnagKey = Data.shared.envValueForKey('BUGSNAG');
 if (BugsnagKey) {
-    Bugsnag = bugsnag({
-        apiKey: BugsnagKey,
-        appVersion: Data.shared.envValueForKey('VERSION'),
-        autoCaptureSessions: true,
-        autoBreadcrumbs: true,
-        networkBreadcrumbsEnabled: true,
-        beforeSend: (report) => {
-            report.user.instance_id = Data.shared.dataId;
-        }
-    });
-    Bugsnag.metaData = {};
+    // Even though we always include bugsnag some evil ad blockers will block
+    // bugsnag even though it is not an ad >:U
+    if (bugsnag) {
+        Bugsnag = bugsnag({
+            apiKey: BugsnagKey,
+            appVersion: Data.shared.envValueForKey('VERSION'),
+            autoCaptureSessions: true,
+            autoBreadcrumbs: true,
+            networkBreadcrumbsEnabled: true,
+            beforeSend: (report) => {
+                report.user.instance_id = Data.shared.dataId;
+            }
+        });
+        Bugsnag.metaData = {};
+    }
 }
 
 /**
