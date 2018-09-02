9299ffbaca72d16fa39c3b39d2b394836a411940
Ryan R <Mego@users.noreply.github.com>
2018-09-02 14:03:42 -0500

    add encodings endpoint (#225)

--- tests/route_tests/test_static.py
+++ tests/route_tests/test_static.py
@@ -3,7 +3,11 @@ from tests.test_base import TestFlask
 # noinspection PyUnresolvedReferences
 import app.routes.static
 
+# noinspection PyUnresolvedReferences
+import app.routes.codepage
+
 import golflang_encodings
+import json
 
 
 class TestStatic(TestFlask):
@@ -20,5 +24,12 @@ class TestStatic(TestFlask):
     def test_codepage(self):
         result = self.client.get('/static/encodings/jelly')
         self.assertEqual(result.status_code, 200)
-        codepage = {int(k):v for k,v in result.json['jelly'].items()}
+        codepage = {int(k): v for k, v in result.json['jelly'].items()}
         self.assertEqual(codepage, golflang_encodings.add_encodings.codepages.get('Jelly'))
+
+    def test_all_encodings(self):
+        result = self.client.get('/static/encodings')
+        self.assertEqual(result.status_code, 200)
+        json_result = json.loads(result.data)
+        encodings = frozenset(json_result['encodings'])
+        self.assertEqual(encodings, app.routes.codepage.ALL_ENCODINGS)
