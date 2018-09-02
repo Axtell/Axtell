bc9ac989f93640f80c8fd0b3159824e9ee6f0f25
Vihan <contact@vihan.org>
2018-09-02 11:57:58 -0700

    Add mobile flow
    Signed-off-by: Vihan <contact@vihan.org>

--- js/ui/write/answer.js
+++ js/ui/write/answer.js
@@ -3,20 +3,22 @@ import FullScreenModalController from '~/controllers/FullScreenModalController';
 import WriteAnswerTemplate from '~/template/WriteAnswer/WriteAnswerTemplate';
 import ErrorManager, { HandleUnhandledPromise } from '~/helpers/ErrorManager';
 
-export const ANSWER_TRIGGER = document.getElementById("write-answer");
+export const ANSWER_TRIGGERS = document.getElementsByClassName("write-answer");
 
-if (ANSWER_TRIGGER) {
-    let template = null;
-    ANSWER_TRIGGER.addEventListener('click', () => {
-        (async () => {
-            if (template === null) {
-                const { default: WriteAnswerTemplate } = await import('~/template/WriteAnswer/WriteAnswerTemplate');
-                template = new WriteAnswerTemplate(Post.current);
-            }
+let template = null;
+const openAnswerTrigger = () => {
+    (async () => {
+        if (template === null) {
+            const { default: WriteAnswerTemplate } = await import('~/template/WriteAnswer/WriteAnswerTemplate');
+            template = new WriteAnswerTemplate(Post.current);
+        }
 
-            FullScreenModalController.shared
-                .present(template)
-                .catch(HandleUnhandledPromise);
-        })();
-    });
-}
+        FullScreenModalController.shared
+            .present(template)
+            .catch(HandleUnhandledPromise);
+    })();
+};
+
+Array.from(ANSWER_TRIGGERS)
+    .map(trigger =>
+        trigger.addEventListener('click', openAnswerTrigger))
